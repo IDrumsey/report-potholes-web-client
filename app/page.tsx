24c77a1d93ab924fb452057daa6919060c5d4771
IDrumsey <claydevelop@gmail.com>
2023-08-05 15:18:01 -0400

    Basic google maps integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,24 @@
-import Image from "next/image"
+"use client"
+
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api"
+import { useState, useCallback } from "react"
 
 export default function Home() {
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY ?? "",
+  })
+
+  const [map, mapSetter] = useState<google.maps.Map | null>()
+
+  const onMapLoad = useCallback(function callback(map: google.maps.Map) {
+    mapSetter(map)
+  }, [])
+
+  const onMapUnmount = useCallback(function callback(map: google.maps.Map) {
+    mapSetter(null)
+  }, [])
+
   return (
     <main className="min-h-screen">
       <div className="w-full lg:grid lg:grid-cols-2 lg:gap-4 flex flex-col gap-0 items-stretch">
@@ -12,7 +30,17 @@ export default function Home() {
             Pothole
           </h1>
         </div>
-        <div className="p-4"></div>
+        <div className="p-4">
+          {isLoaded && (
+            <GoogleMap
+              center={{ lat: 40.676786247456214, lng: -73.96008350857923 }}
+              zoom={17}
+              mapContainerClassName="map-container"
+              onLoad={onMapLoad}
+              onUnmount={onMapUnmount}
+            />
+          )}
+        </div>
       </div>
     </main>
   )
