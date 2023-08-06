fef09c0a3ed481a77351faccbcfc769ccbda0623
IDrumsey <claydevelop@gmail.com>
2023-08-05 20:51:30 -0400

    Added report button

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 "use client"
 
+import styles from "./_home.module.scss"
+
 import { GoogleMap, useJsApiLoader, Marker } from "@react-google-maps/api"
 import { useState, useCallback, useEffect } from "react"
 
@@ -49,31 +51,39 @@ export default function Home() {
   }
 
   return (
-    <main className="min-h-screen">
+    <main className="min-h-screen bg-white px-16 py-10">
       <div className="w-full lg:grid lg:grid-cols-2 lg:gap-4 flex flex-col gap-0 items-stretch">
-        <div className="p-10 pl-20">
+        <div>
           <h1 className="text-left text-7xl font-bold text-[#00A3FF]">
             Report a
           </h1>
-          <h1 className="pl-20 text-8xl mt-6 font-bold text-[#252525]">
+          <h1 className="pl-8 text-8xl mt-6 font-bold text-[#252525]">
             Pothole
           </h1>
         </div>
         <div className="p-4">
           {isLoaded && initialMapCenter && potholeCoords && (
-            <GoogleMap
-              center={initialMapCenter}
-              zoom={17}
-              mapContainerClassName="map-container"
-              onLoad={onMapLoad}
-              onUnmount={onMapUnmount}
-              onClick={onMapClick}
-            >
-              <Marker position={potholeCoords} />
-            </GoogleMap>
-          )}
-          {potholeCoords && (
-            <p className="text-xl">{`${potholeCoords.lat} ${potholeCoords.lng}`}</p>
+            <>
+              <GoogleMap
+                center={initialMapCenter}
+                zoom={17}
+                mapContainerClassName="map-container"
+                onLoad={onMapLoad}
+                onUnmount={onMapUnmount}
+                onClick={onMapClick}
+              >
+                <Marker position={potholeCoords} />
+              </GoogleMap>
+              {potholeCoords && (
+                <p className="text-right text-sm my-5 text-black">{`${potholeCoords.lat} ${potholeCoords.lng}`}</p>
+              )}
+              <div
+                id={styles["report-button"]}
+                className="ml-auto py-5 px-16 bg-white shadow-[0_4px_4px_0_rgba(0,0,0,0.25)] hover:shadow-[0_4px_4px_0_rgba(0,0,0,0.5)] transition-shadow border-solid border-[#F1F1F1] border-1 rounded-md cursor-pointer"
+              >
+                <p className="text-[#00A3FF] font-bold text-2xl">Report</p>
+              </div>
+            </>
           )}
         </div>
       </div>
